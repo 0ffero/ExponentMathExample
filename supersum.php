5508dfb1430c0db50f48f1cd03f687292e39e7d8
0ffero <41750813+0ffero@users.noreply.github.com>
2019-06-17 09:54:49 +0000

    Added text in case of no precision death

--- supersum.php
+++ supersum.php
@@ -16,6 +16,7 @@ $precision = 60;
 
 // These are init vars used by the script
 $br="<br/>";
+$loopCount=256;
 $div = 1;
 $total = 0;
 $precisionDeath = 0;
@@ -25,7 +26,7 @@ bcscale($precision);
 
 // start doing shit
 $opTable = '<table cellpadding="2" cellspacing="4">';
-for ($i=1; $i<256; $i++) {
+for ($i=1; $i<=$loopCount; $i++) {
 	$div = $div*2;
 	$strDiv = number_format($div,0,'','');
 	if (strpos((string)$div, "E")>0) {
@@ -41,7 +42,9 @@ for ($i=1; $i<256; $i++) {
 }
 $opTable .= "</table>";
 
-$header = '<span>Table rows in <span class="precdeath">red</span> show when the maximum precision is inhibiting the ability to add to the total (precision death)</span><br/>Basically the "real division" (and hence the fraction) is so small it will now be rounded to 0 with its precision being a max of ' . $precision . '<br/>Thankfully, before precision death (loop number ' . $precisionDeath . '), we can see a pattern. The number is converging to 0.999\' (that \' means that the 9 never ends no matter the precision)<br/>Some basic rounding on each culculation (after a short time) will leave you with a number between 0.999 (non recurring) and 1. The more loops the more 9\'s in that first number';
+if ($precisionDeath==0) { $precisionDeath = 'No precision death found (loop count of ' . $loopCount . ' is too small to show precision death with a precision of ' . $precision . ')'; } else { $precisionDeath = 'loop number ' . $precisionDeath; }
+
+$header = '<span>Table rows in <span class="precdeath">red</span> show when the maximum precision is inhibiting the ability to add to the total (precision death)</span><br/>Basically the "real division" (and hence the fraction) is so small it will now be rounded to 0 with its precision being a max of ' . $precision . '<br/>Thankfully, before precision death (' . $precisionDeath . '), we can see a pattern. The number is converging to 0.999\' (that \' means that the 9 never ends no matter the precision)<br/>Some basic rounding on each culculation (after a short time) will leave you with a number between 0.999 (non recurring) and 1. The more loops the more 9\'s in that first number';
 
 echo $header;
 echo $opTable;
