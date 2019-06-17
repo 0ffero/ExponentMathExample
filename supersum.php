c6e6b0ac7e3c365b87daa95dc4f417f7c418cf3a
0ffero <41750813+0ffero@users.noreply.github.com>
2019-06-17 10:25:22 +0000

    Add files via upload
    I think this is the final version o.0
    XD

--- supersum.php
+++ supersum.php
@@ -8,6 +8,8 @@
 <style type="text/css">
 table { font-family: monospace; }
 td { border: 1px solid black; }
+td:nth-child(3) { text-align: center; }
+.pdH { background-color: black; color: red; font-variant: small-caps; font-size: 20px; }
 .precdeath { color: red; }
 </style>
 <?php
@@ -16,35 +18,40 @@ $precision = 60;
 $loopCount = 256;
 
 // These are init vars used by the script
-$br="<br/>";
 $div = 1;
 $total = 0;
 $precisionDeath = 0;
+
+$br="<br/>" . PHP_EOL;
 
 // set the precision
 bcscale($precision);
 
 // start doing shit
-$opTable = '<table cellpadding="2" cellspacing="4">';
+$opTable = '<table cellpadding="2" cellspacing="4">' . PHP_EOL . '<tr><td>Loop</td><td>Divider</td><td>Exponent</td><td>Real Divider</td><td>Total</td></tr>' . PHP_EOL;
 for ($i=1; $i<=$loopCount; $i++) {
-	$div = $div*2;
+	$div *= 2;
 	$strDiv = number_format($div,0,'','');
 	if (strpos((string)$div, "E")>0) {
-		// Precision Death;
 		$strpos = strpos((string)$div, "E");
 		$exponent = substr((string)$div,$strpos+2);
-		if ($exponent>=$precision) { $precDeath = ' class="precdeath"'; if ($precisionDeath==0) { $precisionDeath = $i; $opTable .= '<tr style="background-color: black; color: red; font-variant: small-caps; font-size: 20px;"><td colspan="5">Precision Death occurs here</td></tr>'; } }
+		if ($exponent>=$precision) { 
+			$precDeath = ' class="precdeath"';
+			if ($precisionDeath==0) { // Precision Death
+				$precisionDeath = $i; $opTable .= "\t" . '<tr class="pdH"><td colspan="5">Precision Death occurs here</td></tr>' . PHP_EOL;
+			}
+		}
 	} else {
 		$exponent = "none";
 	}
 	$total = bcadd($total, bcdiv("1",$strDiv));
-	$opTable .= "<tr$precDeath><td>Loop#: " . $i . "</td><td>Div: " . $div . "</td><td>Exponent: " . $exponent . "</td><td>Real Div: 1/" . $strDiv . "</td><td>Total: " . $total . "</td></tr>";
+	$opTable .= "\t<tr$precDeath><td>" . $i . "</td><td>" . $div . "</td><td>" . $exponent . "</td><td>1/" . $strDiv . "</td><td>" . $total . "</td></tr>" . PHP_EOL;
 }
 $opTable .= "</table>";
 
 if ($precisionDeath==0) { $precisionDeath = 'No precision death found (loop count of ' . $loopCount . ' is too small to show precision death with a precision of ' . $precision . ')'; } else { $precisionDeath = 'loop number ' . $precisionDeath; }
 
-$header = '<span>Table rows in <span class="precdeath">red</span> show when the maximum precision is inhibiting the ability to add to the total (precision death)</span><br/>Basically the "real division" (and hence the fraction) is so small it will now be rounded to 0 with its precision being a max of ' . $precision . '<br/>Thankfully, before precision death (' . $precisionDeath . '), we can see a pattern. The number is converging to 0.999\' (that \' means that the 9 never ends no matter the precision)<br/>Some basic rounding on each culculation (after a short time) will leave you with a number between 0.999 (non recurring) and 1. The more loops the more 9\'s in that first number';
+$header = '<span>Table rows in <span class="precdeath">red</span> show when the maximum precision is inhibiting the ability to add to the total (precision death)</span>' . $br . 'Basically the "real division" (and hence the fraction) is so small it will now be rounded to 0 with its precision being a max of ' . $precision . $br . 'Thankfully, before precision death (' . $precisionDeath . '), we can see a pattern. The number is converging to 0.999\' (that \' means that the 9 never ends no matter the precision)' . $br . 'Some basic rounding on each culculation (after a short time) will leave you with a number between 0.999 (non recurring) and 1. The more loops the more 9\'s in that first number' . PHP_EOL . PHP_EOL;
 
 echo $header;
 echo $opTable;
